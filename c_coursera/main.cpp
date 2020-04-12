ae9f685e7e59da3fe3a6cf79a2103d8c63c8f2ae
horoshi10v <valentyn.khoroshylov@gmail.com>
2020-04-12 17:54:45 +0300

    Initial Commit

---
+++ c_coursera/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c_coursera
+//
+//  Created by user on 12.04.2020.
+//  Copyright © 2020 valentyn.khoroshylov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
