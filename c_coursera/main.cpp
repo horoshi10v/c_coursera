7fc841fa0d3b2c286df789f6e01af45fb86f9ec5
horoshi10v <valentyn.khoroshylov@gmail.com>
2020-04-12 18:05:23 +0300

    second f in word

--- c_coursera/main.cpp
+++ c_coursera/main.cpp
@@ -6,10 +6,22 @@
 //  Copyright © 2020 valentyn.khoroshylov. All rights reserved.
 //
 
+#include <string>
+#include <math.h>
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    string a;
+    cin>>a;
+    int i=0,b=0;
+    for (auto c : a){
+        if (c =='f'){
+            b++;
+        if(b==2){cout<<i;}
+        }
+        i++;}
+    if (b==1){cout<<"-1";}
+    else if (b==0){cout <<"-2";}
     return 0;
 }
